94249c54f9d6b71477b4021b810504f624aafe8f
sourav <souravdas360@gmail.com>
2023-01-30 22:56:22 +0530

    api loaded

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,29 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd0bc'
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      imageURL: recipe.image_url,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceURL: recipe.source_url,
+      title: recipe.title,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
